44cfdd774c81f19bbf6f4c17d5baa70cdad34e91
Love Lomibao <love@cascadeo.com>
2013-04-24 09:02:12 +0000

    copying the script to libexec

--- ZenPacks/Cascadeo/LDAPMonitorSync/lib/__init__.py
+++ ZenPacks/Cascadeo/LDAPMonitorSync/lib/__init__.py
@@ -1,1 +1,15 @@
 # __init__.py
+import shutil, errno
+import os
+
+def copyanything(src, dst):
+    try:
+        shutil.copytree(src, dst)
+    except OSError as exc: # python >2.5
+        if exc.errno == errno.ENOTDIR:
+            shutil.copy(src, dst)
+        else: raise
+
+src_fn = os.path.dirname(os.path.abspath(__file__)) + "/check_ldap_sync.sh"
+dst_fn = "/usr/local/zenoss/zenoss/libexec/check_ldap_sync.sh"
+copyanything(src_fn, dst_fn)
